fd8fdf67cd4db46061a09071a602d66b52b45f43
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-05 15:41:54 -0500

    Updated bio wording
    Removed list of programs I'm familiar with, as most of them are pretty basic programs.
    
    Fixed a typo in the final line.

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
                     Junior web
                     developer, Aspiring IT
                     technician, and tech hobbyist, looking to find my place in the industry.</h2>
-                <p>My current skillset includes a solid foundation in HTML, CSS, and Javascript. I also know my way
-                    around Cpanel, Github, VS Code, Gimp, and the Google suite. Even outside of work tech is a
+                <p>My current skillset includes a solid foundation in HTML, CSS, and Javascript. Even outside of work
+                    tech is a
                     fundamental part of my life and journey, and is a major factor in me becoming who I am today.
                 </p>
-                <p>IF you'd like more details about who I am and what I can do, please check out any of the other pages
+                <p>If you'd like more details about who I am and what I can do, please check out any of the other pages
                     on my site, or my professional social media pages!
                 </p>
             </div>
