317af2127e9fa052d956556bac86f01fb91a2e4f
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-08 15:45:23 -0500

    Update about.php
    Small formatting changes

--- about.php
+++ about.php
@@ -14,37 +14,40 @@
             <p>I am a Web development and GIS trainee at <a href="https://hopeworks.org/" title="Hopeworks Site"
                     target="_blank" class="link">Hopeworks</a>, I have excelled in my education within the
                 program
-                by channeling my existing knowledge of web development, and my ecstatic passion for tech as a whole. I
-                believe that doing what you love in service of the world you want to create is the most important thing
+                by channeling my existing knowledge of web development, and my ecstatic passion for tech as a whole.
+                I
+                believe that doing what you love in service of the world you want to create is the most important
+                thing
                 a person can do with their lives.</p>
-            <p>I have put in a great deal of effort within the training program, and this has shown in the fact that I
+            <p>I have put in a great deal of effort within the training program, and this has shown in the fact that
+                I
                 have completed the majority of the modules within under a month, all while maintaining my excellent
                 standard of work, and also managing to be awarded trainee of the week within my first month at the
                 company. Currently I am working on my portfolio module, making sure to take my time to ensure it
-                presents my skills in the most accurate and impactful way, this is all part of my eventual goal to move
+                presents my skills in the most accurate and impactful way, this is all part of my eventual goal to
+                move
                 up within the company and secure an internship with one of the professional teams.</p>
             <p>I have a solid foundation of the scripting languages HTML, CSS, Javascript, Zenscript, and JSON. In
                 addition, I also have a basic understanding of the programs Excel, Visual studio Code, Wordpress,
-                ArcGIS, notepad++, Pixlr, and the Google suite. I am also a proud bearer of the Eagle scout rank from my
+                ArcGIS, notepad++, Pixlr, and the Google suite. I am also a proud bearer of the Eagle scout rank
+                from my
                 time in scouting.</p>
             <p>When I am not working on advancing my career in the tech industry, I can still often be found working
                 directly with tech, either playing video games, participating in discussion forums online, or even
                 coding in my spare time for personal projects. Outside of tech, I enjoy tabletop gaming, hiking, and
                 hanging out with my supportive girlfriend and our two cats.</p>
+            <h3 style="text-align: center;">Professional Links and Resume Download</h3>
+            <div class="spotlight-links">
+                <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
+                    download="Kylie Crooker October 2023 Resume"><i class="fa-regular fa-file-lines"></i></a>
+                <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
+                        class="fa-brands fa-linkedin"></i></a>
+                <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
+                        class="fa-brands fa-codepen"></i></a>
+                <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
+                        class="fa-brands fa-github"></i></a>
+            </div>
         </div>
-        <h3 style="text-align: center;">Professional Links and Resume Download</h3>
-        <div class="spotlight-links">
-            <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
-                download="Kylie Crooker October 2023 Resume"><i class="fa-regular fa-file-lines"></i></a>
-            <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
-                    class="fa-brands fa-linkedin"></i></a>
-            <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
-                    class="fa-brands fa-codepen"></i></a>
-            <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
-                    class="fa-brands fa-github"></i></a>
-        </div>
-        <hr style="margin-bottom: 40px;">
-        <div style="width: 100%; height: 50px;"></div>
     </div>
     <?php include("footer.html"); ?>
 </body>
