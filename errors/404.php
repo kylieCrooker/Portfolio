419f8c0832d87fee7d989e01193582bf9eec4202
Rosie-Holstein <kyliecrooker@hopeworks.org>
2023-12-11 19:55:19 -0500

    Create 404.php

---
+++ errors/404.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<head>
+    <?php include("../universalHeadElements.html") ?>
+    <title>Kylie Crooker - 404</title>
+</head>
+
+<html>
+<div id="main">
+    <div class="content-box-error">
+        <h1>ERROR - 404</h1>
+        <h2>The page you attempted to visit doesn't exist, lets just pretend that this didn't happen, huh?</h2>
+        <a class="link" title="Return to Home" tabindex="0" href="/index.php">
+            <h2 style="color:inherit;">Return from whence you came!</h2>
+        </a>
+    </div>
+</div>
+
+</html>
