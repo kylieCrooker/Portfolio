0832aa64369313c9f34102f29c5370ae9f5f8190
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-13 15:38:47 -0500

    Update contact.php
    Reworked page.
    Started implementing a functional contact form.
    added hr tags to spotlight links to mimic example page styling.

--- contact.php
+++ contact.php
@@ -9,37 +9,36 @@
 <body>
     <?php include("header.html"); ?>
     <div id="main">
-        <div class="contact-links">
-            <h2>Contact Me</h2>
-            <a href="mailto:kyliecrooker@hopeworks.org" title="Kyliecrooker@hopeworks.org" class="link"
-                target="_blank">Kyliecrooker@hopeworks.org</a>
-            <a href="tel:1-856-870-0296" title="(856)-870-0296" class="link" target="_blank">(856)-870-0296</a>
+        <div class="content-box-contact">
+            <div class="contact-links">
+                <h2>Contact Me</h2>
+                <a href="mailto:kyliecrooker@hopeworks.org" title="Kyliecrooker@hopeworks.org" class="link"
+                    target="_blank">Kyliecrooker@hopeworks.org</a>
+                <a href="tel:1-856-870-0296" title="(856)-870-0296" class="link" target="_blank">(856)-870-0296</a>
+            </div>
+            <div class="contact-form">
+                <p>SEND EMAIL</p>
+                <form class="contact-form" action="email.php" method="post">
+                    <input type="text" name="name" placeholder="Full name">
+                    <input type="text" name="mail" placeholder="Your e-mail">
+                    <input type="text" name="subject" placeholder="Subject">
+                    <textarea name="message" placeholder="Message"></textarea>
+                    <button type="submit" name="submit">SEND MESSAGE</button>
+                </form>
+            </div>
+            <hr>
+            <div class="spotlight-links">
+                <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
+                    download="Kylie Crooker October 2023 Resume"><i class="fa-regular fa-file-lines"></i></a>
+                <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
+                        class="fa-brands fa-linkedin"></i></a>
+                <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
+                        class="fa-brands fa-codepen"></i></a>
+                <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
+                        class="fa-brands fa-github"></i></a>
+            </div>
+            <hr>
         </div>
-        <div class="contact-form">
-            <form action="email.php" method="post">
-                <label for="fname">Your Name</label>
-                <input id="fname" placeholder="Your name...">
-                <label for="email">Email Address</label>
-                <input id="email" placeholder="A return email address...">
-                <label for="subject">Subject</label>
-                <input id="subject" placeholder="The reason you're sending this message...">
-
-                <label for="message">Your Message</label>
-                <textarea id="message" placeholder="The content of your message..."></textarea>
-            </form>
-        </div>
-        <hr style="margin-bottom: 30px;">
-        <div class="spotlight-links" style="margin-bottom: 40px;">
-            <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
-                download="Kylie Crooker October 2023 Resume"><i class="fa-regular fa-file-lines"></i></a>
-            <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
-                    class="fa-brands fa-linkedin"></i></a>
-            <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
-                    class="fa-brands fa-codepen"></i></a>
-            <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
-                    class="fa-brands fa-github"></i></a>
-        </div>
-        <div style="width: 100%; height: 50px;"></div>
     </div>
     <div class="footer-spacer"></div>
     <?php include("footer.html"); ?>
