5643be91b77335fd465ee598dad46700a8385d18
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-13 15:41:50 -0500

    Create email.php
    Added file to process emails

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_post['name'];
+    $subject = $_post['subject'];
+    $mailFrom = $_post['mail'];
+    $message = $_post['message'];
+
+    $mailTo = "kyliecrooker@hopeworks.org";
+    $headers = "From: " . $mailFrom;
+    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact.php?mailsend");
+}
+
+?>
