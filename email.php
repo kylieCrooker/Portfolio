c522b80b4270a7497b5472a9a07c3c44bb7bdeae
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-14 16:17:38 -0500

    Update email.php
    Fix syntax typo

--- email.php
+++ email.php
@@ -33,7 +33,7 @@ if (isset($_POST['submit'])) {
     $headers = "From: " . $mailFrom;
     $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
 
-    mail($mailTo, "$subject", $txt, $headers);
+    mail($mailTo, $subject, $txt, $headers);
     header("Location: contact.php?mailsend");
     exit;
 }
