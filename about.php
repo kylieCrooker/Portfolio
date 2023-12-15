f07f09f38c8b5a59448d87e62ce95653ec7711b2
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-15 11:15:52 -0500

    Update about.php
    Updated professional bio

--- about.php
+++ about.php
@@ -14,29 +14,34 @@
     <div id="main">
         <div class="content-box-about">
             <h2>About me</h2>
-            <p>I am a web design and development intern at <a href="https://hopeworks.org/" title="Hopeworks Site"
-                    target="_blank" class="link">Hopeworks</a>, I have excelled in my education within the
-                program by channeling my existing knowledge of web development, and my ecstatic passion for tech as a
-                whole. I believe that doing what you love in service of the world you want to create is the most
-                important thing a person can do with their lives.
+            <p>My name is Kylie, and I am a Web design and development intern at <a href="https://hopeworks.org/"
+                    title="Hopeworks Site" target="_blank" class="link">Hopeworks</a>, where I have met and
+                exceeded production goals set out for me, while being driven by my passion for technology and design. I
+                believe that user-friendly design is the cornerstone of any functional website or application, and that
+                doing what you love in service of the world you want to create is the most important thing you can do
+                with your life.</p>
+            <p>I started at hopeworks in their training program, and completed the entire course in less than a month,
+                well ahead of the expected schedule, all while maintaining an excellent standard of work. In addition to
+                this, I received a reward for trainee of the month during my time in the program, as well as several
+                commendations for the work I did in the program.
             </p>
-            <p>I have put in a great deal of effort within the training program, and this has shown in the fact that
-                I
-                have completed the majority of the modules within under a month, all while maintaining my excellent
-                standard of work, and also managing to be awarded trainee of the week within my first month at the
-                company. Currently I am working on my portfolio module, making sure to take my time to ensure it
-                presents my skills in the most accurate and impactful way, this is all part of my eventual goal to
-                move
-                up within the company and secure an internship with one of the professional teams.</p>
-            <p>I have a solid foundation of the scripting languages HTML, CSS, Javascript, Zenscript, and JSON. In
-                addition, I also have a basic understanding of the programs Excel, Visual studio Code, Wordpress,
-                ArcGIS, notepad++, Pixlr, and the Google suite. I am also a proud bearer of the Eagle scout rank
-                from my
-                time in scouting.</p>
+            <p>In my current position, I am responsible for the design of several client pages and site templates,
+                making sure they follow the standards of quality laid out for me, and that they adhere to UI/UX
+                standards, as well following accessibility guidelines. My style of design focuses on a lack of visual
+                clutter, while still maintaining some amount of subtle flair that makes sites feel alive, while still
+                being easy to navigate and not putting strain on potentially lower end hardware or mobile devices.
+            </p>
+            <p>My core languages are CSS, HTML, and Javascript, and I have a solid foundation in Zenscript, PHP, and
+                SCSS. In addition, I have a good amount of experience with Elementor, Figma, VS Code, The Google suite,
+                and Gnu Image Manipulation Program. I am also a proud bearer of the Eagle scout rank from my time in the
+                BSA.
+            </p>
             <p>When I am not working on advancing my career in the tech industry, I can still often be found working
                 directly with tech, either playing video games, participating in discussion forums online, or even
                 coding in my spare time for personal projects. Outside of tech, I enjoy tabletop gaming, hiking, and
-                hanging out with my supportive girlfriend and our two cats.</p>
+                hanging out with my supportive girlfriend and our two cats.
+
+            </p>
             <h3 style="text-align: center;">Professional Links and Resume Download</h3>
             <hr>
             <div class="spotlight-links">
