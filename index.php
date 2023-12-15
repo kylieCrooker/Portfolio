34d909c79627d7c5a8aa3d2419ba267741eed891
Kylie Crooker <kyliecrooker@hopeworks.org>
2023-12-15 11:19:22 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,14 +16,16 @@
                     Junior web
                     developer, Aspiring IT
                     technician, and tech hobbyist, looking to find my place in the industry.</h2>
-                <h2>Core Languages: HTML, CSS, Javascript</h2>
-                <h2>Program Experience: VS Code, Elementor, Figma, Google suite</h2>
-                <p>Even outside of work tech is a fundamental part of my life and journey, and is a major factor in me
-                    becoming who I am today.
-                </p>
-                <p>If you'd like more details about who I am and what I can do, please check out any of the other pages
-                    on my site, or my professional social media pages!
-                </p>
+                <h3>Core Languages: HTML, CSS, Javascript</h2>
+                    <h3>Program Experience: VS Code, Elementor, Figma, Google suite</h2>
+                        <p>Even outside of work tech is a fundamental part of my life and journey, and is a major factor
+                            in me
+                            becoming who I am today.
+                        </p>
+                        <p>If you'd like more details about who I am and what I can do, please check out any of the
+                            other pages
+                            on my site, or my professional social media pages!
+                        </p>
             </div>
             <img src="images/Kylie_cropped.JPG">
         </div>
