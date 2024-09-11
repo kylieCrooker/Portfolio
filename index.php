4831ce66d2fba8bbd66004071b8074a954592526
Rosie-Holstein <kyliecrooker@hopeworks.org>
2024-09-11 14:54:27 -0400

    Update index.php

--- index.php
+++ index.php
@@ -12,22 +12,36 @@
         <div class="content-box-home">
             <div class="text-box">
                 <h2>Hey there! I'm <a href="https://en.pronouns.page/@kyliecrooker#she/her" class="link" target="_blank"
-                        title="Pronouns page">Kylie Crooker</a>, a
-                    Junior web
-                    developer, Aspiring IT
-                    technician, and tech hobbyist, looking to find my place in the industry.</h2>
-                <h3>Core Languages: HTML, CSS, Javascript</h2>
-                    <h3>Program Experience: VS Code, Elementor, Figma, Google suite</h2>
+                        title="Pronouns page">Kylie Crooker</a>, a Technical Support Specialist with hands-on experience
+                    in troubleshooting and resolving technical issues, as well as having <a href="/examples.php"
+                        title="Portfolio" class="link">plenty of UI/UX design experience.</a></h2>
+                    <hr>
+                <h3>Core Competencies: HTML, JavaScript, CSS, Bash, Linux, SSH, Apache2, Nginx</h2>
+                <h3>Program Experience: VS Code, Elementor, Figma, Google suite</h2>
+                    <hr>
                         <p>Even outside of work tech is a fundamental part of my life and journey, and is a major factor
-                            in me
-                            becoming who I am today.
+                            in me becoming who I am today.
                         </p>
-                        <p>If you'd like more details about who I am and what I can do, please check out any of the
-                            other pages
-                            on my site, or my professional social media pages!
+                        <p>If you'd like more details about who I am and my experience, please check out my <a
+                                href="/about.php" title="About Me" class="link">About Me Page</a>, or my <a
+                                href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn"
+                                tabindex="0" class="link">LinkedIn Profile</a>.
                         </p>
             </div>
             <img src="images/Kylie_cropped.JPG">
+        </div>
+    </div>
+    <div class="link-box-general">
+        <hr>
+        <div class="spotlight-links">
+            <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
+                download="Kylie Crooker Resume - September 2024.pdf"><i class="fa-regular fa-file-lines"></i></a>
+            <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
+                    class="fa-brands fa-linkedin"></i></a>
+            <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
+                    class="fa-brands fa-codepen"></i></a>
+            <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
+                    class="fa-brands fa-github"></i></a>
         </div>
     </div>
     <div class="footer-spacer"></div>
