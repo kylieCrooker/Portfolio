406ee7ae7e21f4981586a46ea11fd173e2c97673
Rosie-Holstein <kyliecrooker@hopeworks.org>
2024-09-11 14:54:06 -0400

    Update about.php

--- about.php
+++ about.php
@@ -14,39 +14,65 @@
     <div id="main">
         <div class="content-box-about">
             <h2>About me</h2>
-            <p>My name is Kylie, and I am a Web design and development intern at <a href="https://hopeworks.org/"
-                    title="Hopeworks Site" target="_blank" class="link">Hopeworks</a>, where I have met and
-                exceeded production goals set out for me, while being driven by my passion for technology and design. I
-                believe that user-friendly design is the cornerstone of any functional website or application, and that
-                doing what you love in service of the world you want to create is the most important thing you can do
-                with your life.</p>
-            <p>I started at hopeworks in their training program, and completed the entire course in less than a month,
-                well ahead of the expected schedule, all while maintaining an excellent standard of work. In addition to
-                this, I received a reward for trainee of the month during my time in the program, as well as several
-                commendations for the work I did in the program.
+            <p>My name is Kylie, and I am a Technical support professional, most recently having worked at <a
+                    href="https://www.akamai.com/" title="Akamai Webpage" target="_blank" class="link">Akamai</a> in
+                their cloud computing division. I believe that technology and systems should be built with people in
+                mind, rather than asking them to adapt to what has already been built.
             </p>
-            <p>In my current position, I am responsible for the design of several client pages and site templates,
-                making sure they follow the standards of quality laid out for me, and that they adhere to UI/UX
-                standards, as well following accessibility guidelines. My style of design focuses on a lack of visual
-                clutter, while still maintaining some amount of subtle flair that makes sites feel alive, while still
-                being easy to navigate and not putting strain on potentially lower end hardware or mobile devices.
+            <p>Throughout my career I have put
+                this belief into practice by providing excellent customer service and tailored recommendations while
+                working as a sales associate at GameStop, and by taking user experience and accessibility into account
+                while doing web design work during my time at Hopeworks. I further exemplified this belief by focusing
+                on quality over quantity with my tickets, anticipating further customer questions, and providing
+                additional resources as needed, all while communicating in a friendly and conversational manner.</p>
+            <hr>
+            <p>I started at Akamai Compute (Previously known as Linode) as part of their 6-month internship program,
+                while there I met and exceeded the expectations of my trainers and managers, both during my training and
+                after entering the support queue. Using the tools available to me, I performed troubleshooting of
+                technical and billing issues to provide solutions that were designed to minimize the effort of both
+                customers and future team members that may work on the same ticket. I plan to develop my career in
+                technical support and gain more experience in the field, with a goal to eventually shift into systems
+                administration.
             </p>
-            <p>My core languages are CSS, HTML, and Javascript, and I have a solid foundation in Zenscript, PHP, and
-                SCSS. In addition, I have a good amount of experience with Elementor, Figma, VS Code, The Google suite,
-                and Gnu Image Manipulation Program. I am also a proud bearer of the Eagle scout rank from my time in the
-                BSA.
+            <p>While at Akamai, I worked as part of the support team, focusing on billing and
+                account tickets. In addition to this, we were also provided with a set of continuing education projects
+                to familiarize ourselves with the tools that we would be using during our six months with the team, as
+                well as the tools our customers would be using on our platform. While participating in these projects, I
+                was able to complete both the mandatory projects as well as any extra credit projects which were
+                assigned to us, making me the first intern to complete all the available projects in the program's
+                2-year history.
             </p>
+            <hr>
+            <p>My core competencies are Linux, web design, Visual Studio Code, technical support, customer service, and
+                UE/UX design. In addition to my existing experience with these concepts, I also am adept at finding my
+                way around new systems, and using available knowledge bases to quickly learn and understand new tools
+                and environments. This is a large part of what allowed me to complete every extra credit assignment
+                while at Akamai Compute, as the self-paced course allowed me to take full advantage of my ability to
+                quickly learn and adapt to new technologies and concepts.
+            </p>
+            <p>In addition to my technical skills, my
+                time in the Boy Scouts of America (BSA) provided me with greater confidence and self-reliance as we
+                learned skills such as wilderness survival, conservation, and leadership. I was able to combine all of
+                these skills, as well as my technical knowledge in my eagle project, where I worked with a local church
+                to create a database of all the individuals buried at their cemetery, as well as other information on
+                their headstones, such as dates and memorials. This project resulted in me being awarded my Eagle rank
+                in May of 2018.
+            </p>
+            <hr>
             <p>When I am not working on advancing my career in the tech industry, I can still often be found working
-                directly with tech, either playing video games, participating in discussion forums online, or even
-                coding in my spare time for personal projects. Outside of tech, I enjoy tabletop gaming, hiking, and
-                hanging out with my supportive girlfriend and our two cats.
-
+                directly with tech, playing video games, or participating in discussion forums online. I even make use
+                of my technical experience in my day-to-day life, using Linux as one of my daily drivers, coding for
+                personal projects, troubleshooting computer problems for friends, and building and upgrading my own
+                computer. Outside of tech, I enjoy tabletop gaming, such as Dungeons and Dragons, Magic the Gathering,
+                or a wide variety of other games. I also enjoy hiking, sketching, reading, and hanging out with my
+                girlfriend and life partner Amilia, who is a great source of support and joy in my life, along with our
+                cat, Goblin.
             </p>
             <h3 style="text-align: center;">Professional Links and Resume Download</h3>
             <hr>
             <div class="spotlight-links">
                 <a href="Kylie Crooker October 2023 Resume.pdf" target="_blank" title="My Resume" class="link"
-                    download="Kylie Crooker October 2023 Resume"><i class="fa-regular fa-file-lines"></i></a>
+                    download="Kylie Crooker Resume - September 2024.pdf"><i class="fa-regular fa-file-lines"></i></a>
                 <a href="https://www.linkedin.com/in/kylie-crooker/" target="_blank" title="My LinkedIn" class="link"><i
                         class="fa-brands fa-linkedin"></i></a>
                 <a href="https://codepen.io/Kylie-Crooker" target="_blank" title="My Codepen" class="link"><i
@@ -54,7 +80,6 @@
                 <a href="https://github.com/kylieCrooker" target="_blank" title="My Github" class="link"><i
                         class="fa-brands fa-github"></i></a>
             </div>
-            <hr>
         </div>
     </div>
     <div class="footer-spacer"></div>
